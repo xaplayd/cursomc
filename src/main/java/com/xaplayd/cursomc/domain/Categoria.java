00afe3b24d6a3d025998576217fd36d05753ab14
xaplayd <douglas_comelli_balbinot@hotmail.com>
2024-10-14 14:49:22 -0300

    Implemented categorias domain

---
+++ src/main/java/com/xaplayd/cursomc/domain/Categoria.java
@@ -0,0 +1,62 @@
+package com.xaplayd.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer id;
+	private String name;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+}
--- src/main/java/com/xaplayd/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/xaplayd/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.xaplayd.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.xaplayd.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "teste implantação REST";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCat = new ArrayList<>();
+		listaCat.add(cat1);
+		listaCat.add(cat2);
+		
+		return listaCat;
 	}
 
 }
